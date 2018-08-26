ada9208249af43ae8f961e30680c4bb30b56ec04
sdMac <553828127@qq.com>
2018-08-26 23:22:27 +0800

    add life page

---
+++ src/app/content/life/life.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { LifeComponent } from './life.component';
+import { ButtonModule } from 'src/app/basic/button/button.module';
+
+@NgModule({
+  imports: [
+  ],
+  exports: [ LifeComponent ],
+  declarations: []
+})
+
+export class LifeModule { }
